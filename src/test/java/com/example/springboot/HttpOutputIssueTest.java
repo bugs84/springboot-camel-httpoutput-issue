231f092bbc29df57eabd8a26b27b4e29a1d13913
Jan Vondrouš <j.vondrous@quadient.com>
2021-09-02 14:26:50 +0200

    Sample test, which show the issue Bug: ISC-23020

--- src/test/java/com/example/springboot/HttpOutputIssueTest.java
+++ src/test/java/com/example/springboot/HttpOutputIssueTest.java
@@ -1,43 +1,39 @@
 package com.example.springboot;
 
+import com.jcabi.http.request.JdkRequest;
+import com.jcabi.http.response.RestResponse;
 import org.apache.camel.test.spring.CamelSpringBootRunner;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 
+import static com.jcabi.http.Request.GET;
+import static java.net.HttpURLConnection.HTTP_OK;
+import static org.hamcrest.CoreMatchers.equalTo;
+
 @RunWith(CamelSpringBootRunner.class)
 @SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class HttpOutputIssueTest/* extends CamelSpringTestSupport */{
-
-    @Autowired
-    private ServerProperties serverProperties;
-
-//    @Autowired
-//    private ServletWebServerApplicationContext webServerAppCtxt;
+class HttpOutputIssueTest {
 
     @LocalServerPort
-    int randomServerPort;
+    int port;
 
-//        @LocalManagementPort
-//        int randomManagementPort;
-
+    /**
+     * This test works with Camel 3.10.0, but fails with 3.11.0 and 3.11.1
+     *
+     * Looks like some sort of issue with aggregator
+     **/
     @Test
-    void configure() throws InterruptedException {
-        Integer port = serverProperties.getPort();
-
-//        int port2 = webServerAppCtxt.getWebServer().getPort();
-
-        System.out.println("PORT: " + port + "   , 2="+randomServerPort);
-        Thread.sleep(20_000);
+    void httpIssue() throws Exception {
+        String urlString = "http://localhost:" + port + "/camel/httpIssue";
+        new JdkRequest(urlString)
+                .method(GET)
+                .fetch()
+                .as(RestResponse.class)
+                .assertStatus(HTTP_OK)
+                .assertBody(equalTo("Response 0"))
+        ;
     }
 
-//    @Override
-//    protected AbstractApplicationContext createApplicationContext() {
-//        AbstractApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
-//
-//        return context;
-//    }
 }
